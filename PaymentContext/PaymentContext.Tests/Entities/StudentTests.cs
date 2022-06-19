848ee0b1cf17268f2f7c6c32a83a9f59f4922788
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-19 10:05:08 -0300

    Adicionando as validações

--- PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests;
 
@@ -10,6 +11,7 @@ public class StudentTests
     public void TestMethod1()
     {
 
-
+        var name = new Name("Teste", "Joao");
+        name.Notifications
     }
 }
