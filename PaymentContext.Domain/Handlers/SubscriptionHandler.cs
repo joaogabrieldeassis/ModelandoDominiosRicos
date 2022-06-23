babf2de393ae998c6ba9760d8da6151b11aade61
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-23 09:24:14 -0300

    Criando um fluxo e injeção de dependencia

--- PaymentContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -12,128 +12,40 @@ using PaymentContext.Shared.Handlers;
 
 namespace PaymentContext.Domain.Handlers
 {
-    public class SubscriptionHandler :
-        Notifiable,
-        IHandler<CreateBoletoSubscriptionCommand>,
-        IHandler<CreatePayPalSubscriptionCommand>
+    public class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>
     {
         private readonly IStudentRepository _repository;
-        private readonly IEmailService _emailService;
-
-        public SubscriptionHandler(IStudentRepository repository, IEmailService emailService)
+        public SubscriptionHandler(IStudentRepository repository)
         {
             _repository = repository;
-            _emailService = emailService;
         }
-
         public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
         {
-            // Fail Fast Validations
+            //Fail Fast Validations 
             command.Validate();
             if (command.Invalid)
             {
                 AddNotifications(command);
-                return new CommandResult(false, "Não foi possível realizar sua assinatura");
+                return new CommandResult(false, "Não foi possivel realizar o cadastro");
             }
-
-            // Verificar se Documento já está cadastrado
+            //Verificar se docomumento e email já está cadastrado
             if (_repository.DocumentExists(command.Document))
-                AddNotification("Document", "Este CPF já está em uso");
+                AddNotification("Document", "Este documento já existe");
 
-            // Verificar se E-mail já está cadastrado
             if (_repository.EmailExists(command.Email))
-                AddNotification("Email", "Este E-mail já está em uso");
+                AddNotification("Email", "Este email já existe");
 
-            // Gerar os VOs
+            //Gerar as VOs
             var name = new Name(command.FirstName, command.LastName);
             var document = new Document(command.Document, EDocumentType.CPF);
             var email = new Email(command.Email);
-            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
-
-            // Gerar as Entidades
-            var student = new Student(name, document, email);
-            var subscription = new Subscription(DateTime.Now.AddMonths(1));
-            var payment = new BoletoPayment(
-                command.BarCode,
-                command.BoletoNumber,
-                command.PaidDate,
-                command.ExpireDate,
-                command.Total,
-                command.TotalPaid,
-                command.Payer,
-                new Document(command.PayerDocument, command.PayerDocumentType),
-                address,
-                email
-            );
-
-            // Relacionamentos
-            subscription.AddPayment(payment);
-            student.AddSubscription(subscription);
-
-            // Agrupar as Validações
-            AddNotifications(name, document, email, address, student, subscription, payment);
-
-            // Checar as notificações
-            if (Invalid)
-                return new CommandResult(false, "Não foi possível realizar sua assinatura");
-
-            // Salvar as Informações
-            _repository.CreateSubscription(student);
-
-            // Enviar E-mail de boas vindas
-            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo ao balta.io", "Sua assinatura foi criada");
-
-            // Retornar informações
-            return new CommandResult(true, "Assinatura realizada com sucesso");
-        }
-
-        public ICommandResult Handle(CreatePayPalSubscriptionCommand command)
-        {
-            // Verificar se Documento já está cadastrado
-            if (_repository.DocumentExists(command.Document))
-                AddNotification("Document", "Este CPF já está em uso");
-
-            // Verificar se E-mail já está cadastrado
-            if (_repository.EmailExists(command.Email))
-                AddNotification("Email", "Este E-mail já está em uso");
-
-            // Gerar os VOs
-            var name = new Name(command.FirstName, command.LastName);
-            var document = new Document(command.Document, EDocumentType.CPF);
-            var email = new Email(command.Email);
-            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
-
-            // Gerar as Entidades
-            var student = new Student(name, document, email);
-            var subscription = new Subscription(DateTime.Now.AddMonths(1));
-            // Só muda a implementação do Pagamento
-            var payment = new PayPalPayment(
-                command.TransactionCode,
-                command.PaidDate,
-                command.ExpireDate,
-                command.Total,
-                command.TotalPaid,
-                command.Payer,
-                new Document(command.PayerDocument, command.PayerDocumentType),
-                address,
-                email
-            );
-
-            // Relacionamentos
-            subscription.AddPayment(payment);
-            student.AddSubscription(subscription);
-
-            // Agrupar as Validações
-            AddNotifications(name, document, email, address, student, subscription, payment);
-
-            // Salvar as Informações
-            _repository.CreateSubscription(student);
-
-            // Enviar E-mail de boas vindas
-            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo ao balta.io", "Sua assinatura foi criada");
-
-            // Retornar informações
-            return new CommandResult(true, "Assinatura realizada com sucesso");
+            var adrress = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+            //Gerar as entidades 
+            //Aplicar as validações
+            //Salvar as informações
+            //Enviar email de boas vindas 
+            //Retornar informações 
+            return new CommandResult(true, "Assinaura realizada com sucesso");
         }
     }
 }
